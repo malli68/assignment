cb975039edb1c4d9ca860ea23086357bd4353175
mallikarjun <mallikarjun.vemala@mobileprogramming.com>
2024-01-15 00:42:21 +0530

    no data

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -1,36 +1,45 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { TABLE_HEADERS } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const EditForm = () => {
   const userInfo = useSelector((store) => store.user);
   console.log(userInfo.user);
   return (
+    
     <>
-        <table>
-        <thead>
-          <tr>
-          {
-            TABLE_HEADERS.map((header)=>{
-              return <th key={header}>{header}</th>
-            })
-          }
-          </tr>
-        </thead>
-        <tbody>
-        {userInfo.length>0 ? 
-         (userInfo.map((row,index)=>(
-          <tr>
-          {TABLE_HEADERS.map((column, index)=>{
-           return <td key={row.name}>{row[column]}</td>
-          })}
-          </tr>
-        ))
+    {userInfo.length > 0 ? (
+
+<table>
+<thead>
+  <tr>
+  {
+    TABLE_HEADERS.map((header)=>{
+      return <th key={header}>{header}</th>
+    })
+  }
+  </tr>
+</thead>
+<tbody>
+{userInfo.map((row,index)=>(
+  <tr>
+  {TABLE_HEADERS.map((column, index)=>{
+   return <td key={row.name}>{row[column]}</td>
+  })}
+  </tr>
+))
+}
+</tbody>
+</table>
+    ):(
+      <div className="Table">
+        <p> no data here click on this button to create user </p>
+        <Link to="/create"><button>Add User</button></Link>
+      </div>
       )
-      : <p className="nodata">No data</p>
     }
-        </tbody>
-      </table>
+
     </>
   );
 };
